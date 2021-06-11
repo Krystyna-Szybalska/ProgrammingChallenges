34f5e00eaba04c637e8eb5587f97cf5379d46919
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-06-11 14:25:57 +0200

    Started working on name generator

---
+++ 00_Name_Generator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _00_Name_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //imię ma być fajne xD
+            // wymyśliłabym jakąś zasadę, w stylu każde męskie imię kończy się na iks, a żeńskie na ina
+            // naprzemiennie spółgłoska i samogłoska
+            // ogarnąć częstotliwość występowania? np. a na końcu częściej?
+            // Is it a male or female? (M/F)
+            // His/Her name will be...
+        }
+    }
+}
